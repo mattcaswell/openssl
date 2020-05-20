1098cde1d57c8142cbad6b3481d61d13b42c1039
Matt Caswell <matt@openssl.org>
2020-05-20 14:46:22 +0100

    Make EVP_PKEY_CTX_[get|set]_group_name work for DH too
    The previous commit added the EVP_PKEY_CTX_[get|set]_group_name
    functions to work with EC groups. We now extend that to also work for
    DH.

--- include/openssl/core_names.h
+++ include/openssl/core_names.h
@@ -195,7 +195,7 @@ extern "C" {
 #define OSSL_PKEY_PARAM_MGF1_DIGEST         "mgf1-digest"
 #define OSSL_PKEY_PARAM_MGF1_PROPERTIES     "mgf1-properties"
 #define OSSL_PKEY_PARAM_TLS_ENCODED_PT      "tls-encoded-pt"
-#define OSSL_PKEY_PARAM_GROUP_NAME          "group-name"
+#define OSSL_PKEY_PARAM_GROUP_NAME          "group"
 
 /* Diffie-Hellman/DSA public/private key */
 #define OSSL_PKEY_PARAM_PUB_KEY             "pub"
@@ -212,7 +212,6 @@ extern "C" {
 #define OSSL_PKEY_PARAM_FFC_H        "hindex"
 
 /* Diffie-Hellman params */
-#define OSSL_PKEY_PARAM_DH_GROUP     "group"
 #define OSSL_PKEY_PARAM_DH_GENERATOR "safeprime-generator"
 #define OSSL_PKEY_PARAM_DH_PRIV_LEN   "priv_len"
 
